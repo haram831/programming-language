5b8eaaacd163253ba90b8c7a4445039e6ec44155
김하람 <gim222932@gmail.com>
2026-03-21 19:49:08 +0900

    feat: implement tree zipper

---
+++ hw2/assignment4.ml
@@ -0,0 +1,30 @@
+type item = string
+type tree = LEAF of item
+| NODE of tree list
+type zipper = TOP
+| HAND of tree list * zipper * tree list
+type location = LOC of tree * zipper
+exception NOMOVE of string
+
+let goLeft loc = 
+  match loc with
+  | LOC(t, TOP) -> raise (NOMOVE "left of top")
+  | LOC(t, HAND(l::left, up, right)) -> LOC(l, HAND(left, up, t::right))
+  | LOC(t, HAND([], up, right))-> raise (NOMOVE "left of first")
+
+let goRight loc =
+  match loc with
+  | LOC(t, TOP) -> raise (NOMOVE "right of top")
+  | LOC(t, HAND(left, up, r::right))-> LOC(r, HAND(t::left, up, right)) (*left::t의 형태가 아니어도 되나..?*)
+  | LOC(t, HAND(_, _, [])) -> raise (NOMOVE "right of first")
+
+let goUp loc =
+  match loc with
+  | LOC(t, TOP) -> raise (NOMOVE "up of top")
+  | LOC(t, HAND(left, up, right)) -> LOC( NODE (List.rev left @ (t::right)) , up) 
+
+let goDown loc =
+  match loc with
+  | LOC(LEAF _, _) -> raise (NOMOVE "down of leaf")
+  | LOC(NODE(t::ts), z) -> LOC(t, HAND([], z, ts))
+  | LOC(NODE [], _) -> raise (NOMOVE "down of empty node")
